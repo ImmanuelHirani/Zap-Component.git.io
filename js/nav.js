c9c0f42208b21fcf9ac23360251bd49d9e61b3a1
immanuelHirani <gabutb688@gmail.com>
2023-10-03 17:44:34 +0700

    p

--- js/nav.js
+++ js/nav.js
@@ -1,69 +1,65 @@
-const navBurger = document.querySelector("#navBurger");
-const Language = document.querySelector("#Language");
-const Languagesm = document.querySelector("#Languagesm");
-const LanguageBox = document.querySelector("#LanguageBox");
-const LanguageBoxsm = document.querySelector("#LanguageBoxsm");
-const navDropTreatment = document.querySelector("#navDropTreatment");
-const navmenuTreatment = document.querySelector("#navmenuTreatment");
-const btntreatmentSM = document.querySelector("#btn-treatmentSM");
-const treatbtnlink = document.querySelector("#treatbtnlink");
-const navexpand = document.querySelector("#navexpand");
+const dropdownNavs = document.querySelectorAll('.dropdownNav');
+const navContainer = document.querySelector('.navContainer');
 
+let activeDropdownNav = null; // Variabel untuk melacak elemen yang sedang aktif
 
-navBurger.addEventListener("click", function () {
-  navBurger.classList.toggle("navBurger-active");
-});
+dropdownNavs.forEach((dropdownNav) => {
+  dropdownNav.addEventListener('mouseenter', () => {
+    // Menambahkan event listener mouseenter
+    if (activeDropdownNav) {
+      // Hapus kelas aktif dari elemen yang sedang aktif sebelumnya
+      activeDropdownNav.classList.remove('navDrop-Active');
+      const activeDropMenuId = activeDropdownNav.getAttribute('data-drop-menu');
+      const activeDropMenu = document.getElementById(activeDropMenuId);
+      activeDropMenu.classList.remove('dropMenu-Active');
+      activeDropMenu.classList.add('opacity-0');
+    }
 
-navDropTreatment.addEventListener("click", function () {
-  navDropTreatment.classList.toggle("navDropTreatment-active");
-  navexpand.classList.toggle("navexpand-active");
-  navmenuTreatment.classList.toggle("hidden");
-});
+    dropdownNav.classList.add('navDrop-Active');
+    const dropMenuId = dropdownNav.getAttribute('data-drop-menu');
+    const dropMenu = document.getElementById(dropMenuId);
+    dropMenu.classList.add('dropMenu-Active');
+    dropMenu.classList.remove('opacity-0');
+    navContainer.classList.add('navExpands-Active');
 
-// Offcanvas js sm
-btntreatmentSM.addEventListener("click", function () {
-  btntreatmentSM.classList.toggle("btntreatmentSM-active");
-  treatbtnlink.classList.toggle("hidden");
-});
-// End
-
-Language.addEventListener("click", function () {
-  Language.classList.toggle("Language-active");
-  LanguageBox.classList.toggle("language-box-active");
-});
-
-Languagesm.addEventListener("click", function () {
-  Languagesm.classList.toggle("Language-active");
-  LanguageBoxsm.classList.toggle("hidden");
-});
-
-
-function changeLanguage(lang) {
-  const languageImg = document.querySelector("#Language img");
-  const checkENG = document.getElementById("checkENG");
-  const checkID = document.getElementById("checkID");
-  const languageBox = document.getElementById("LanguageBox");
-
-  // Menghapus kelas "Language-active" dari semua tombol bahasa
-  const languageButtons = document.querySelectorAll(".language-Wrapper button");
-  languageButtons.forEach(button => {
-    button.classList.remove("Language-active");
+    activeDropdownNav = dropdownNav; // Set elemen yang sedang aktif
   });
+});
 
-  if (lang === "ENG") {
-    languageImg.src = "temp-asset/statis-asset/English-Bahasa.png";
-    checkENG.innerHTML = `<i class='text-2xl bx bx-check'></i>`;
-    checkID.innerHTML = `<i class='text-2xl bx'></i>`;
-  } else if (lang === "ID") {
-    languageImg.src = "temp-asset/statis-asset/Indo-Bahasa.png";
-    checkENG.innerHTML = `<i class='text-2xl bx'></i>`;
-    checkID.innerHTML = `<i class='text-2xl bx bx-check'></i>`;
+navContainer.addEventListener('mouseleave', () => {
+  if (activeDropdownNav) {
+    activeDropdownNav.classList.remove('navDrop-Active');
+    const activeDropMenuId = activeDropdownNav.getAttribute('data-drop-menu');
+    const activeDropMenu = document.getElementById(activeDropMenuId);
+    activeDropMenu.classList.remove('dropMenu-Active');
+    activeDropMenu.classList.add('opacity-0');
+    activeDropdownNav = null; // Reset elemen yang sedang aktif
   }
+  navContainer.classList.remove('navExpands-Active');
+});
 
-  // Menambahkan kelas "Language-active" ke tombol yang diklik
-  const selectedButton = document.querySelector(`.language-Wrapper button[data-lang='${lang}']`);
-  selectedButton.classList.add("Language-active");
 
-  // Hide language dropdown
-  languageBox.classList.add("hidden");
-}
+
+// Menambahkan event listener mouseleave pada navContainer
+// navContainer.addEventListener('mouseleave', (e) => {
+//     // Mendapatkan posisi mouse saat mouseleave terjadi
+//     const mouseX = e.clientX;
+//     const mouseY = e.clientY;
+
+//     // Mendapatkan posisi dan ukuran dari dropMenu
+//     const dropMenuRect = dropMenu.getBoundingClientRect();
+
+//     // Memeriksa apakah mouse berada di luar dropMenu
+//     if (
+//         mouseX < dropMenuRect.left ||
+//         mouseX > dropMenuRect.right ||
+//         mouseY < dropMenuRect.top ||
+//         mouseY > dropMenuRect.bottom
+//     ) {
+//         // Mouse berada di luar dropMenu, menghilangkan kelas aktif
+//         dropdownNavs.classList.remove('navDrop-Active');
+//         dropMenu.classList.remove('dropMenu-Active');
+//         dropMenu.classList.add('opacity-0');
+//         navContainer.classList.remove('navExpands-Active');
+//     }
+// });
--- js/solution.js
+++ js/solution.js
@@ -1,81 +1,27 @@
 const treatmentChoose = document.querySelectorAll(".treatment-choose");
-const box = document.querySelectorAll(".box-choise-wrapper");
+const box = document.querySelector(".box-choise-wrapper");
 const myButtons = document.querySelectorAll('.myButton');
-const findFitTreatmentButtons = document.querySelectorAll('.find-fit-treatment');
+const findFitTreatmentButtons = document.querySelector('.find-fit-treatment');
 const treatmentFit = document.querySelectorAll('.treatment-fit');
 
-treatmentChoose.forEach((treatmentChooseBTN) => {
-  treatmentChooseBTN.addEventListener("click", () => {
-    // Menghapus kelas aktif dari semua elemen treatmentChoose
-    treatmentChoose.forEach((trBtn) => {
-      if (trBtn !== treatmentChooseBTN) {
-        trBtn.classList.remove("treatment-active");
-      }
-    });
-
-    // Menghapus kelas hidden dan menambahkan kelas box-Active pada semua box
-    box.forEach((boxItem) => {
-      boxItem.classList.remove("hidden");
-      boxItem.classList.add("box-Active");
-
-      // Secara eksplisit mengatur max-height saat menghapus kelas hidden
-      boxItem.style.maxHeight = boxItem.scrollHeight + "px";
-    });
-
-    // Toggle class pada elemen yang diklik
-    treatmentChooseBTN.classList.toggle("treatment-active");
-
-    // Cek apakah ada elemen treatment-choose yang aktif
-    const isActive = Array.from(treatmentChoose).some((trBtn) =>
-      trBtn.classList.contains("treatment-active")
-    );
-
-    // Jika tidak ada elemen yang aktif, tambahkan kembali kelas hidden pada semua box
-    if (!isActive) {
-      box.forEach((boxItem) => {
-        boxItem.classList.add("hidden");
-        boxItem.style.maxHeight = "0";
+treatmentChoose.forEach((othertreatment) => {
+  othertreatment.addEventListener('click', () => {
+    // Periksa apakah elemen yang diklik memiliki kelas 'tc-active'
+    if (othertreatment.classList.contains('tc-active')) {
+      othertreatment.classList.remove('tc-active');
+    } else {
+      // Hapus kelas 'tc-active' dari semua elemen dalam treatmentChoose
+      treatmentChoose.forEach((element) => {
+        element.classList.remove('tc-active');
       });
+      othertreatment.classList.add('tc-active');
+    }
+
+    // Periksa apakah setidaknya satu elemen dalam treatmentChoose memiliki kelas 'tc-active'
+    if (document.querySelector('.treatment-choose.tc-active')) {
+      box.classList.add('box-active');
+    } else {
+      box.classList.remove('box-active');
     }
   });
 });
-
-
-myButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    // Toggle class on the clicked button
-    button.classList.toggle('button-choose-active');
-    
-    // Check if any of the myButton elements have the 'button-choose-active' class
-    const isActive = Array.from(myButtons).some((btn) => btn.classList.contains('button-choose-active'));
-
-    // Enable or disable all find-fit-treatment buttons based on the isActive variable
-    findFitTreatmentButtons.forEach((findFitTreatmentButton) => {
-      if (isActive) {
-        findFitTreatmentButton.removeAttribute('disabled');
-        findFitTreatmentButton.classList.add('button-fit-treatment-active'); // Use 'classList.add' to add a class
-      } else {
-        findFitTreatmentButton.setAttribute('disabled', 'disabled');
-        findFitTreatmentButton.classList.remove('button-fit-treatment-active'); // Use 'classList.remove' to remove a class
-      }
-    });
-  });
-});
-
-findFitTreatmentButtons.forEach((button) => {
-    button.addEventListener('click',() => {
-        button.classList.toggle('fit-treatment-active')
-
-        const isActive = Array.from(findFitTreatmentButtons).some((btn) => btn.classList.contains('fit-treatment-active'));
-
-        treatmentFit.forEach((button)=> {
-            if(isActive) {
-                button.classList.remove('hidden');
-            } else {
-                button.classList.add('hidden')
-            }
-        })
-
-
-    })
-})
