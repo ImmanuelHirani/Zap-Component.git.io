fc24752868378a2ef1f683c94077aa3ad83c4166
immanuelHirani <gabutb688@gmail.com>
2023-10-15 11:41:25 +0700

    p

---
+++ js/locations.js
@@ -0,0 +1,10 @@
+const zap = document.getElementById("zap");
+const zapPremire = document.getElementById("zapPremiere");
+const zapManlogy = document.getElementById("zapManlogy");
+const zapLoc = document.getElementById("zapLoc");
+const zapPremireLoc = document.getElementById("zapPremiereLoc");
+const zapManlogyLoc = document.getElementById("zapManlogyLoc");
+
+zap.addEventListener('click',()=> {
+    zapLoc.classList.toggle('hidden')
+})
