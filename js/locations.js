e3ab85e68958970b193b31b075af9325d88219e2
immanuelHirani <gabutb688@gmail.com>
2023-10-15 14:46:54 +0700

    p

--- js/locations.js
+++ js/locations.js
@@ -2,9 +2,23 @@ const zap = document.getElementById("zap");
 const zapPremire = document.getElementById("zapPremiere");
 const zapManlogy = document.getElementById("zapManlogy");
 const zapLoc = document.getElementById("zapLoc");
-const zapPremireLoc = document.getElementById("zapPremiereLoc");
+const zapPremireLoc = document.getElementById("zapPremierLoc");
 const zapManlogyLoc = document.getElementById("zapManlogyLoc");
 
 zap.addEventListener('click',()=> {
-    zapLoc.classList.toggle('hidden')
+    zapLoc.classList.remove('hidden')
+    zapPremireLoc.classList.add('hidden')
+    zapManlogyLoc.classList.add('hidden')
+})
+
+zapPremire.addEventListener('click',()=> {
+    zapLoc.classList.add('hidden')
+    zapPremireLoc.classList.remove('hidden')
+    zapManlogyLoc.classList.add('hidden')
+})
+
+zapManlogy.addEventListener('click',()=> {
+    zapLoc.classList.add('hidden')
+    zapPremireLoc.classList.add('hidden')
+    zapManlogyLoc.classList.remove('hidden')
 })
