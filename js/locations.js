351314f2ea948fe0669c4a5e27617c3d72028f8a
immanuelHirani <gabutb688@gmail.com>
2023-10-18 07:49:45 +0700

    p

--- js/locations.js
+++ js/locations.js
@@ -1,19 +1,25 @@
-function toggleElement(element, elements) {
-    elements.forEach(el => el.classList.add('opacity-25'));
-    element.classList.remove('opacity-25');
-}
-
-function toggleLocation(location, locations) {
-    locations.forEach(loc => loc.classList.add('hidden'));
-    location.classList.remove('hidden');
-}
-
 const zap = document.getElementById("zap");
 const zapPremire = document.getElementById("zapPremiere");
 const zapManlogy = document.getElementById("zapManlogy");
 const zapLoc = document.getElementById("zapLoc");
 const zapPremireLoc = document.getElementById("zapPremierLoc");
 const zapManlogyLoc = document.getElementById("zapManlogyLoc");
+const Scheduletreatment = document.querySelectorAll(".scheduletreatment");
+const ModalTreatment = document.getElementById("ModalTreatment");
+const closeModalButton = document.querySelector(".closeModal");
+
+
+Scheduletreatment.forEach((otherScheduleTreatment) => {
+    otherScheduleTreatment.addEventListener('click', () => {
+         ModalTreatment.classList.remove("scale-0");
+         ModalTreatment.classList.add("ModalActive");
+    })
+})
+
+closeModalButton.addEventListener('click', () => {
+    ModalTreatment.classList.add("scale-0")
+    ModalTreatment.classList.remove("ModalActive");
+})
 
 zap.addEventListener('click', () => {
     toggleElement(zap, [zapPremire, zapManlogy]);
@@ -29,3 +35,14 @@ zapManlogy.addEventListener('click', () => {
     toggleElement(zapManlogy, [zap, zapPremire]);
     toggleLocation(zapManlogyLoc, [zapLoc, zapPremireLoc]);
 });
+
+
+function toggleElement(element, elements) {
+    elements.forEach(el => el.classList.add('opacity-25'));
+    element.classList.remove('opacity-25');
+}
+
+function toggleLocation(location, locations) {
+    locations.forEach(loc => loc.classList.add('hidden'));
+    location.classList.remove('hidden');
+}
